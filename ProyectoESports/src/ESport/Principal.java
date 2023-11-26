45f2159211595252159f35a30eab66489cbac075
Julian Bahamondes <cachululop7@gmail.com>
2023-11-25 21:17:31 -0300

    Se hace metodo para buscar jugador

--- ProyectoESports/src/ESport/Principal.java
+++ ProyectoESports/src/ESport/Principal.java
@@ -7,69 +7,69 @@ import java.util.Scanner;
  *
  * @author fgom9
  */
-public class Principal {
+//public class Principal {
 
-    public static void main(String[] args) throws Exception {
-
-        Equipo equi = new Equipo();
-        Metodo met = new Metodo();
-        Scanner print = new Scanner(System.in);
-        Jugador jug = new Jugador();
-        while (true) {
-            System.out.println("ESport");
-            System.out.println("1. Crear equipo");
-            System.out.println("2. Crear Jugador");
-            System.out.println("3. Modificar equipo");
-            System.out.println("4. Agregar jugadores a equipo");
-            System.out.println("5. Listar equipos");
-            System.out.println("6. Salir");
-            System.out.print("Seleccione una opción: ");
-            int opcion = print.nextInt();
-            print.nextLine();
-
-            switch (opcion) {
-                case 1:
-                    met.agregarEquipo();
-                    break;
-                case 2:
-                    met.agregarJugador();
-                    break;
-                case 3:
-                    met.actualizarEquipo();
-
-                    break;
-                case 4:
-                    met.agregarJugadorAEquipo();
-
-                    break;
-
-                case 5:
-                    met.actualizarJugador();
-                    break;
-
-                case 6:
-                    met.eliminarJugador();
-                    break;
-                    case 7:
-                    met.eliminarEquipo();
-                    break;
-                    case 8:
-                    met.mostrarEquipo();
-                    break;
-                    case 9:
-                    met.mostrarJugador();
-                    break;
-                    case 10:
-                    met.mostrarEquipoJug();
-                    break;
+//    public static void main(String[] args) throws Exception {
+//
+//        Equipo equi = new Equipo();
+//        Metodo met = new Metodo();
+//        Scanner print = new Scanner(System.in);
+//        Jugador jug = new Jugador();
+//        while (true) {
+//            System.out.println("ESport");
+//            System.out.println("1. Crear equipo");
+//            System.out.println("2. Crear Jugador");
+//            System.out.println("3. Modificar equipo");
+//            System.out.println("4. Agregar jugadores a equipo");
+//            System.out.println("5. Listar equipos");
+//            System.out.println("6. Salir");
+//            System.out.print("Seleccione una opción: ");
+//            int opcion = print.nextInt();
+//            print.nextLine();
+//
+//            switch (opcion) {
+//                case 1:
+//                    met.agregarEquipo();
+//                    break;
+//                case 2:
+//                    met.agregarJugador();
+//                    break;
+//                case 3:
+//                    met.actualizarEquipo();
+//
+//                    break;
+//                case 4:
+//                    met.agregarJugadorAEquipo();
+//
+//                    break;
+//
+//                case 5:
+//                    met.actualizarJugador();
+//                    break;
+//
 //                case 6:
-//                    System.out.println("¡Hasta pronto!");
-//                    System.exit(0);
-//                default:
-//                    System.out.println("Opción no válida.");
+//                    met.eliminarJugador();
+//                    break;
+//                    case 7:
+//                    met.eliminarEquipo();
+//                    break;
+//                    case 8:
+//                    met.mostrarEquipo();
+//                    break;
+//                    case 9:
+//                    met.mostrarJugador();
+//                    break;
+//                    case 10:
+//                    met.mostrarEquipoJug();
+//                    break;
+////                case 6:
+////                    System.out.println("¡Hasta pronto!");
+////                    System.exit(0);
+////                default:
+////                    System.out.println("Opción no válida.");
+////            }
+////        }
 //            }
 //        }
-            }
-        }
-    }
-}
+//    }
+//}
--- ProyectoESports/src/vista/VistaBuscarEquipo.java
+++ ProyectoESports/src/vista/VistaBuscarEquipo.java
@@ -32,6 +32,7 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -50,17 +51,14 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(129, 129, 129)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(142, 142, 142))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(14, 14, 14))
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
@@ -91,6 +89,13 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("jButton2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -104,14 +109,20 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jButton1)))
                 .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(232, 232, 232)
+                .addComponent(jButton2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 128, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 269, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton2)
+                .addGap(8, 8, 8)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -124,7 +135,7 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -135,6 +146,10 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
         mForm9.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -188,6 +203,7 @@ public class VistaBuscarEquipo extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
